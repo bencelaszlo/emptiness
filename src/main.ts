4432095ecf363030bd820b1c3b45c773aba9ad02
bencelaszlo <bencelaszlo@protonmail.com>
2024-04-13 21:15:04 +0200

    change: append canvas to #app

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { Application, Sprite, Assets } from 'pixi.js';
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
 const app = new Application();
 
-// Wait for the Renderer to be available
 await app.init();
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
-document.body.appendChild(app.canvas);
+document.querySelector('#app')?.appendChild(app.canvas);
 
 // load the texture we need
 const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
